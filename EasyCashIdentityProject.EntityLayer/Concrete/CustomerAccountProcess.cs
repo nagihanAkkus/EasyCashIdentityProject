88255f31b57c247bafd32d40c7dee4b5db84cbef
nagihanakkus <nagihan.akkus.76@gmail.com>
2024-02-24 17:19:10 +0300

    feat: created EntityLayer

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+    }
+}
+
+/*
+ ID - işlem Türü(Gelen, Giden, ödeme) - Miktar - Tarih - Alıcı - Gönderici
+ */
